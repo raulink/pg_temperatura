c158ce5399b99a1be85b1a1e1cac5bd5469e8d03
raulink <mc.raul86@gmail.com>
2022-08-25 20:43:16 -0400

    Funcionando lcd, habia un problema con la libreria, todo OK, lectura basica sensor, muestra i2c, envia serial 9600

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 
 #include <LiquidCrystal_I2C.h>
 #include <SPI.h>
-//#include "../lib/temperatura/temperatura.h"
 #include "max6675.h"
 
-//LiquidCrystal_I2C lcd (0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+int lcdColumns = 16;
+int lcdRows = 2;
 
-LiquidCrystal_I2C lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-//LiquidCrystal_I2C lcd(0x38);
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+
 int ktcSO = 12;
 int ktcCS = 13;
 int ktcCLK = 14;
@@ -20,21 +20,22 @@ MAX6675 termocupla(ktcCLK, ktcCS, ktcSO);
 void setup() {
   
   Serial.begin(9600);
-  Serial.println("MAX6675 test");
-  lcd.begin(16, 2);  
+  Serial.println("MAX6675 test");  
+  lcd.init();
+  lcd.backlight();
   Serial.begin(9600);
   Serial.println("MAX6675 test");
   delay(500); 
 }
 
-void loop() {
-  // basic readout test, just print the current temperature  
+void loop() {  
   
-   lcd.clear(); 
-   lcd.print("T1 "); 
+   //lcd.clear(); 
+   lcd.setCursor(0, 0);   
+   lcd.print("Temperatura: "); 
+   lcd.setCursor(0,1);
    lcd.print(termocupla.readCelsius());
    Serial.println(termocupla.readCelsius());
-   lcd.setCursor(0,1);
-   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
+   // 250ms entre lecturas!
    delay(500);
 }
